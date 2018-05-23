ff8e8587f889653502b27e196c30e95860939bd3
Eduardo Reis <edureissantos007@gmail.com>
2018-05-23 12:23:09 -0300

    Configurando o layout

--- src/main/java/com/edujavaonline/brewer/config/WebConfig.java
+++ src/main/java/com/edujavaonline/brewer/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -91,6 +92,14 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		resolver.setTemplateMode(TemplateMode.HTML);
 		return resolver;
 
+	}
+	
+	/**
+	 * Através deste método, conseguimos adicionar recurso(imagens, CSS, Javascript, etc) que não tenham o controller
+	 */
+	@Override	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("**/").addResourceLocations("classpath:/static/");
 	}
 
 }
--- src/main/java/com/edujavaonline/brewer/controller/CervejasController.java
+++ src/main/java/com/edujavaonline/brewer/controller/CervejasController.java
@@ -31,4 +31,9 @@ public class CervejasController {
 		System.out.println(">>>>>>descrição:" + cerveja.getDescricao());
 		return "redirect:/cervejas/novo";
 	}
+	
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
+	}
 }
