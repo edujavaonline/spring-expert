e8ed6700826a0b5e83da4cf0e5218710679072f9
Eduardo Reis <edureissantos007@gmail.com>
2018-05-28 10:10:14 -0300

    Prototipando o cadastro de cliente

---
+++ src/main/java/com/edujavaonline/brewer/controller/ClientesController.java
@@ -0,0 +1,13 @@
+package com.edujavaonline.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientesController {
+	
+	@RequestMapping("/clientes/novo")
+	public String novo() {
+		return "cliente/CadastroCliente";
+	}
+}
