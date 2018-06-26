24152c28e5f6a43751321d0d45bc1ecd13ba0a93
Eduardo Reis <edureissantos007@gmail.com>
2018-06-26 16:32:27 -0300

    Montando os combos e radio do cadastro da cerveja

--- src/main/java/com/edujavaonline/brewer/controller/CervejasController.java
+++ src/main/java/com/edujavaonline/brewer/controller/CervejasController.java
@@ -8,25 +8,31 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.edujavaonline.brewer.model.Cerveja;
-import com.edujavaonline.brewer.repository.Cervejas;
+import com.edujavaonline.brewer.model.Origem;
+import com.edujavaonline.brewer.model.Sabor;
+import com.edujavaonline.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 	
 	@Autowired
-	private Cervejas cervejas;
+	private Estilos estilos;
 
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		cervejas.findAll();
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {		
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method=RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			return novo(cerveja);
 		}
@@ -35,7 +41,7 @@ public class CervejasController {
 		System.out.println(">>>>>>sku:" + cerveja.getSku());
 		System.out.println(">>>>>>nome:" + cerveja.getNome());
 		System.out.println(">>>>>>descrição:" + cerveja.getDescricao());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}	
 	
 }
